2b944228c3120e68e5625c4f1e60025fd66859e9
Raul Ferreira <105366476+RaulFerreiraSilva@users.noreply.github.com>
2023-03-28 22:02:45 -0300

    Retorno da api azure congnitive Ok

--- SmartBulaSite/Controllers/HomeController.cs
+++ SmartBulaSite/Controllers/HomeController.cs
@@ -9,6 +9,11 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json.Linq;
 
 namespace SmartBulaSite.Controllers
 {
@@ -26,14 +31,58 @@ namespace SmartBulaSite.Controllers
             return View();
         }
 
-        public IActionResult Busca()
+        public IActionResult Buscar()
         {
             return View();
         }
 
-        public IActionResult Buscar(string principio_ativo)
+        [HttpPost]
+        public async Task<IActionResult> Buscar(string principio_ativo)
         {
+            
+
+
+            foreach (IFormFile arq in Request.Form.Files)
+            {
+                string tipoArquivo = arq.ContentType;
+                string extensao = System.IO.Path.GetExtension(arq.FileName);
+
+                if (tipoArquivo.Contains("image"))
+                {//se for imagem eu vou gravar no banco
+                    MemoryStream s = new MemoryStream();
+                    arq.CopyTo(s);
+                    byte[] bytesArquivo = s.ToArray();
+                    string content = await MakeRequest(bytesArquivo);
+                    dynamic stuff = JsonConvert.DeserializeObject(content);
+                    principio_ativo = stuff.readResult.content;
+                }
+                else
+                {
+                    principio_ativo = "Não foi possivel encontrar o nome do remedio!";
+                }
+            }
             return View(Remedio.BuscarRemedio(principio_ativo));
+        }
+
+        static async Task<String> MakeRequest(byte[] img)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "6c193a3b7d2747ae8fc02707a665fb7f");
+
+            var uri = "https://scanremedio.cognitiveservices.azure.com/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=read&";
+
+            HttpResponseMessage response;
+
+            using (var content = new ByteArrayContent(img))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(uri, content);
+                string responseSucess = await response.Content.ReadAsStringAsync();
+                return responseSucess;
+            }
         }
 
         public IActionResult Privacy()
