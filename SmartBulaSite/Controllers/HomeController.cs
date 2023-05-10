a7c7ec6d4dc086ebbf1d2495a7f2362ff50bbe0c
MichelFM <123601902+TatiOlive@users.noreply.github.com>
2023-05-09 22:12:42 -0300

    Paginas Lincadas
    linkei todos os href da home e curiosidades

--- SmartBulaSite/Controllers/HomeController.cs
+++ SmartBulaSite/Controllers/HomeController.cs
@@ -61,6 +61,7 @@ namespace SmartBulaSite.Controllers
         {
             return View();
         }
+       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
