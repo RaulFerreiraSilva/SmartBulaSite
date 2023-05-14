b633ad0850cd5f48fdc8948c3b18d250e474cf47
Polycarpo <105468470+Polycarpoo@users.noreply.github.com>
2023-05-13 23:52:26 -0300

    Código Ok
    concertando o código quebrado

--- SmartBulaSite/wwwroot/js/site.js
+++ SmartBulaSite/wwwroot/js/site.js
@@ -1,1 +1,30 @@
-﻿
+﻿//Coloca o botão em uma varivel
+var btn_subir = $(".btn");
+
+
+//Faz a primeira verificacao ao carregar a pagina
+$(document).ready(function () {
+    var minhaposicao = $(this).scrollTop();
+    if (minhaposicao >= 100) {
+        btn_subir.fadeIn();
+    }
+    else {
+        btn_subir.fadeOut();
+    }
+});
+
+//Fica monitorando a rolagem de pagina
+$(window).scroll(function () {
+    var minhaposicao = $(this).scrollTop();
+
+    if (minhaposicao >= 100) {
+        btn_subir.fadeIn();
+    }
+    else {
+        btn_subir.fadeOut();
+    }
+});
+
+btn_subir.click(function () {
+    $('html,body').animate({ scrollTop: 0 }, 500);
+})
